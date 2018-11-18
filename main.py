4844568edb45440bc6048ec2473c32d790a250b5
Spencer <spencertperry23@gmail.com>
2018-11-18 14:02:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-int main()
+'''
+CS 454 Fall 2018
+Ryan Boyle, Spencer Perry, and John Salman
+Final Project
+--------------------------------------
+Overview:
+--------------------------------------
+Report:
+
+'''
+
+
+
+def main():
+
+    print('hello world')
 
 main()
