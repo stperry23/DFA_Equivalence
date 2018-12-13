91041eb2590e21361b600264af188758905f6211
john-salman <johnfish15@gmail.com>
2018-12-12 18:07:38 -0800

    Added some stuff for optimizing, untested, commented out a new interface

--- union.py
+++ union.py
@@ -50,9 +50,23 @@ class UNION_FIND:
             return i
         return self.find_parent(self.collection[i])
 
+    def find_parent_compress(self, i):
+        if (self.collection[i] == -1):
+            return i
+        else:
+            self.collection[i] = self.find_parent_compress(self.collection[i])
+     
     def union(self,x,y):
         self.collection[x] = y
 
+    def union_by_height(self, x, y):
+        if (self.collection[y] < self.collection[x]):
+            self.collection[x] = y
+        else:
+            if (self.collection[x] == self.collection[y]):
+                self.collection[x] -= 1
+            self.collection[y] = x
+            
     def equivalence(self):
         print "Our DFA:"
         for i in range(len(self.dfa_main.delta)):
@@ -70,6 +84,54 @@ class UNION_FIND:
         print self.collection
 
 
+    def equivalence_height(self):
+        print "Our DFA:"
+        for i in range(len(self.dfa_main.delta)):
+            print "accept: ", self.dfa_main.delta[i].accepting, " zero: ", self.dfa_main.delta[i].zero_tr, " one: ", self.dfa_main.delta[i].one_tr
+        while (len(self.List) != 0):
+            current = self.List.pop(0)
+            print "LEEST:", self.List
+            A_1 = self.find_parent(current[0])
+            A_2 = self.find_parent(current[1])
+            if A_1 != A_2:
+                self.union_by_height(A_1, A_2)
+                self.List.append([self.dfa_main.delta[current[0]].zero_tr, self.dfa_main.delta[current[1]].zero_tr])
+                self.List.append([self.dfa_main.delta[current[0]].one_tr, self.dfa_main.delta[current[1]].one_tr])
+        print self.collection
+
+    def equivalence_compress(self):
+        print "Our DFA:"
+        for i in range(len(self.dfa_main.delta)):
+            print "accept: ", self.dfa_main.delta[i].accepting, " zero: ", self.dfa_main.delta[i].zero_tr, " one: ", self.dfa_main.delta[i].one_tr
+        while (len(self.List) != 0):
+            current = self.List.pop(0)
+            print "LEEST:", self.List
+            A_1 = self.find_parent_compress(current[0])
+            A_2 = self.find_parent_compress(current[1])
+            if A_1 != A_2:
+                self.union(A_1, A_2)
+                self.List.append([self.dfa_main.delta[current[0]].zero_tr, self.dfa_main.delta[current[1]].zero_tr])
+                self.List.append([self.dfa_main.delta[current[0]].one_tr, self.dfa_main.delta[current[1]].one_tr])
+        print self.collection
+
+    def equivalence_height_compress(self):
+        print "Our DFA:"
+        for i in range(len(self.dfa_main.delta)):
+            print "accept: ", self.dfa_main.delta[i].accepting, " zero: ", self.dfa_main.delta[i].zero_tr, " one: ", self.dfa_main.delta[i].one_tr
+        while (len(self.List) != 0):
+            current = self.List.pop(0)
+            print "LEEST:", self.List
+            A_1 = self.find_parent_compress(current[0])
+            A_2 = self.find_parent_compress(current[1])
+            if A_1 != A_2:
+                self.union_by_height(A_1, A_2)
+                self.List.append([self.dfa_main.delta[current[0]].zero_tr, self.dfa_main.delta[current[1]].zero_tr])
+                self.List.append([self.dfa_main.delta[current[0]].one_tr, self.dfa_main.delta[current[1]].one_tr])
+        print self.collection
+
+
+                                                                                                                                                                         
+        
 class LAZY:
     def __init__(self, dfa_file_1, dfa_file_2):
         self.dfa_1 = DFA(dfa_file_1)
@@ -108,9 +170,37 @@ class LAZY:
              print "NARC"
              
 def main():
-    #DFA_1_FILE = input("DFA 1: ")
-    #DFA_2_FILE = input("DFA 2: ")
+    '''
+    DFA_1_FILE = input("Please type the name of the file containing the first DFA: ")
+    DFA_2_FILE = input("Please type the name of the file containing the second DFA: ")
 
+    LAZY_obj = LAZY(DFA_1_FILE, DFA_2_FILE)
+    UNION_FIND_obj = UNION_FIND(DFA_1_FILE, DFA_2_FILE)
+    
+    prog_decision = 1
+    while(prog_decision != -1):
+        alg_decision = input("Enter 1 for lazy, 2 for union_find: ")
+        if (alg_decision == 1):
+            LAZY_obj.exclusive()
+        if (alg_decision == 2):
+            union_decision = input("Enter 1 for default Union_Find, 2 for Union by Height, 3 for Union with path compression, 4 for Union by Height with path compression: ")
+            if (union_decision == 1):
+                UNION_FIND_obj.equivalence()
+            elif (union_decision == 2):
+                UNION_FIND_obj.equivalence_height()
+            elif (union_decision == 3):
+                UNION_FIND_obj.equivalence_compress()
+            elif (union_decision == 4):
+                UNION_FIND_obj.equivalence_height_compress()
+            else:
+                UNION_FIND_obj.equivalence()
+        prog_decision = input("Enter -1 to quit, 1 to continue with same input, 0 to change input: ")
+        if (prog_decision == 0):
+             DFA_1_FILE = input("Enter file 1: ")
+             DFA_2_FILE = input("Enter file 2: ")
+             LAZY_obj = LAZY(DFA_1_FILE, DFA_2_FILE)
+             UNION_FIND_obj = UNION_FIND(DFA_1_FILE, DFA_2_FILE)
+    '''
     decision = input("Enter 1 for lazy, 2 for union_find: ")
     if (decision == 1):
         LAZY_obj = LAZY(sys.argv[1], sys.argv[2])
